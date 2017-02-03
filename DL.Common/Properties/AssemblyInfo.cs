351f97767a4a7a452b4453da45b75ef5bdf1b1f4
knight0323 <jeremy@theknights.me>
2017-02-03 11:31:59 -0600

    added nuspec file

--- DL.Common/Properties/AssemblyInfo.cs
+++ DL.Common/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DL.Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A utility library containing boilerplate code, extension methods, new classes, and/or improvements to existing classes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Digital Lagniappe")]
 [assembly: AssemblyProduct("DL.Common")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Digital Lagniappe. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
