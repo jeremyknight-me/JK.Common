eaaa8622496d6d96072f1239b792f93688781b47
Jeremy <jeremy@theknights.me>
2017-06-09 11:18:03 -0500

    added DateTimeProvider and DateTimeOffsetProvider to allow for unit testing the 2 types

---
+++ DL.Common/TypeHelpers/DateTimeProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DL.Common.TypeHelpers
+{
+    public abstract class DateTimeProvider
+    {
+        private static DateTimeProvider current = DefaultDateTimeProvider.Instance;
+
+        public static DateTimeProvider Current
+        {
+            get => current;
+            set => current = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public abstract DateTime Now { get; }
+
+        public abstract DateTime UtcNow { get; }
+
+        public static void ResetToDefault()
+        {
+            current = DefaultDateTimeProvider.Instance;
+        }
+    }
+}
---
+++ DL.Common/TypeHelpers/DefaultDateTimeOffsetProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DL.Common.TypeHelpers
+{
+    public sealed class DefaultDateTimeOffsetProvider : DateTimeOffsetProvider
+    {
+        #region Singleton Pattern Implementation
+
+        private static readonly object threadLock = new object();
+
+        private static DefaultDateTimeOffsetProvider instance;
+
+        private DefaultDateTimeOffsetProvider()
+        {
+        }
+
+        public static DefaultDateTimeOffsetProvider Instance
+        {
+            get
+            {
+                lock (threadLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DefaultDateTimeOffsetProvider();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        public override DateTimeOffset Now => DateTimeOffset.Now;
+        public override DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+    }
+}
