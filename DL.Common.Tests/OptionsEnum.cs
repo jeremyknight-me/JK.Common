6c777dcb508915e96f08b0a7fe6d0bd37de99da4
Jeremy <jeremy@theknights.me>
2017-08-21 13:56:44 -0500

    added test to cover read-only properties

---
+++ DL.Common.Tests/OptionsEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace DL.Common.Tests
+{
+    public enum OptionsEnum
+    {
+        Option1,
+        Option2
+    }
+}
--- DL.Common.Tests/Text/TemplateProcessorTests.cs
+++ DL.Common.Tests/Text/TemplateProcessorTests.cs
@@ -1,4 +1,5 @@
 ﻿using DL.Common.Text;
+using System;
 using Xunit;
 
 namespace DL.Common.Tests.Text
@@ -77,12 +78,41 @@ namespace DL.Common.Tests.Text
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void ProcessTemplate_ReadOnlyDisplayProperty_ReplacesToken()
+        {
+            var testObject = new TemplateTester { Option = OptionsEnum.Option1 };
+            string template = "The option is {{TemplateTester.OptionDisplay}}.";
+            var templateProcessor = new TemplateProcessor(template);
+            templateProcessor.Objects.Add(testObject);
+            string actual = templateProcessor.ProcessTemplate();
+            Assert.Equal("The option is Option 1.", actual);
+        }
+
         #endregion
 
         private class TemplateTester
         {
             // ReSharper disable once UnusedAutoPropertyAccessor.Local
             public string StringValue { get; set; }
+
+            public OptionsEnum Option { get; set; }
+
+            public string OptionDisplay
+            {
+                get
+                {
+                    switch (this.Option)
+                    {
+                        case OptionsEnum.Option1:
+                            return "Option 1";
+                        case OptionsEnum.Option2:
+                            return "Option 2";
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
         }
     }
 }
