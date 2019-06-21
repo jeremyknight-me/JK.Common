c1ff65a15e0a5f444354e3624461568b4bb6a474
Jeremy <jeremy@theknights.me>
2019-06-20 23:58:09 -0500

    added ConcurrentBagExtensions

---
+++ JK.Common/Extensions/ConcurrentBagExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JK.Common.Extensions
+{
+    public static class ConcurrentBagExtensions
+    {
+        public static void AddRange<T>(this ConcurrentBag<T> bag, IEnumerable<T> list)
+        {
+            _ = Parallel.ForEach(list, (item) =>
+            {
+                bag.Add(item);
+            });
+        }
+    }
+}
