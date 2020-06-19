36ff0dbe7c6685633600f485b12c148e99534371
Jeremy Knight <jeremy.knight@cgb.com>
2020-06-19 16:18:01 -0500

    Added generic ADO classes for reference purposes (migrated from older code)

--- src/Common/CHANGELOG.md
+++ src/Common/CHANGELOG.md
@@ -15,6 +15,7 @@ Unless otherwise noted, all changes made by [@jeremyknight-me](https://github.co
 - Added DoesImplement<T> to TypeExtensions.
 - Added GetAttribute<T> to EnumHelper.
 - Added new helper class ExcelHelper with GetColumnName(int columnNumber). 
+- Added generic ADO classes for reference purposes (migrated from older code). 
 
 ## [1.0.6] - 05 Oct 2019
 
---
+++ src/Common/JK.Common/Data/AdoBaseOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace JK.Common.Data
+{
+    [Obsolete("Generic ADO classes have not been tested in .NET Core. Left in for reference purposes.")]
+    public abstract class AdoBaseOperation
+    {
+        private readonly AdoBaseRepository repository;
+
+        /// <summary>
+        /// Initializes a new instance of the AdoBaseOperation class.
+        /// </summary>
+        /// <param name="adoBaseRepository">The ADO base repository to use.</param>
+        protected AdoBaseOperation(AdoBaseRepository adoBaseRepository)
+        {
+            this.repository = adoBaseRepository;
+        }
+
+        protected AdoBaseRepository Repository => this.repository;
+
+        protected abstract void SetupParameters(DbCommand command);
+
+        protected AdoParameterFactory GetParameterFactory(IDbCommand command)
+        {
+            return this.repository.GetParameterFactory(command);
+        }
+
+        protected DbCommand SetupCommand(DbConnection connection, CommandType commandType, string commandText)
+        {
+            DbCommand command = connection.CreateCommand();
+            command.CommandType = commandType;
+            command.CommandText = commandText;
+            this.SetupParameters(command);
+            return command;
+        }
+    }
+}
