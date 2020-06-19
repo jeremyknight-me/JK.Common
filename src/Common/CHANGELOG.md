5e055ffcecf27b36d0e0f98890d85fbf791eed75
Jeremy Knight <jeremy.knight@cgb.com>
2020-06-19 15:24:40 -0500

    Added DoesImplement<T> to TypeExtensions

--- src/Common/CHANGELOG.md
+++ src/Common/CHANGELOG.md
@@ -5,6 +5,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+Unless otherwise noted, all changes made by [@jeremyknight-me](https://github.com/jeremyknight-me).
+
 ## [Unreleased]
 
 ### Added
@@ -16,10 +18,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Added AddWorkDays function to DateTimeHelper.
+- Added DoesImplement<T> to TypeExtensions.
 
 ### Changed
 - Updated email validation regex.
-- Upgraded .NET version from Core 2.1 to Core 3.0.
+- Upgraded .NET version to Core 3.0.
 
 [Unreleased]: https://github.com/jeremyknight-me/JK.Common/compare/1.0.6...HEAD
 [1.0.6]: https://github.com/jeremyknight-me/JK.Common/compare/1.0.5...1.0.6
--- src/Common/JK.Common/Extensions/TypeExtensions.cs
+++ src/Common/JK.Common/Extensions/TypeExtensions.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Linq;
 
 namespace JK.Common.Extensions
 {
     public static class TypeExtensions
     {
+        /// <summary>
+        /// Determines whether or not a type implements the given interface.
+        /// </summary>
+        /// <typeparam name="T">Interface type to find.</typeparam>
+        /// <param name="type">Type to check for the interface.</param>
+        /// <returns>True if implemented, otherwise false.</returns>
+        public static bool DoesImplement<T>(this Type type) 
+        {
+            var interfaceType = typeof(T);
+            if (!interfaceType.IsInterface)
+            {
+                throw new ArgumentException("Only interfaces can be passed as T.");
+            }
+
+            return type.GetInterfaces().Any(x => x.FullName == interfaceType.FullName); ;
+        }
+
         public static Type GetTypeFromEntity(this Type type)
         {
             if (type.BaseType != null && type.Namespace == "System.Data.Entity.DynamicProxies")
