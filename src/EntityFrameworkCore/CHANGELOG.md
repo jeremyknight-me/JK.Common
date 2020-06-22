fdb3f3868c62ae0c00d04c95817c3a81e4712595
Jeremy Knight <jeremy.knight@cgb.com>
2020-06-22 16:17:00 -0500

    Added EFCore and EFCore.SqlServer libraries. Added ReadOnlyDbContext to EFCore library. Added PropertyBuilderExtensions to EFCore.SqlServer library.

---
+++ src/EntityFrameworkCore/CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+Unless otherwise noted, all changes made by [@jeremyknight-me](https://github.com/jeremyknight-me).
+
+## [Unreleased]
+
+### Added
+
+- Added EFCore and EFCore.SqlServer libraries.
+- Added ReadOnlyDbContext to EFCore library.
+- Added PropertyBuilderExtensions to EFCore.SqlServer library.
---
+++ src/EntityFrameworkCore/JK.Common.EntityFrameworkCore/ReadOnlyDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JK.Common.EntityFrameworkCore
+{
+    public abstract class ReadOnlyDbContext<TContext> : DbContext
+    {
+        protected ReadOnlyDbContext()
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected ReadOnlyDbContext(DbContextOptions options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        #region SaveChanges overrides - ensures read-only
+
+        public override int SaveChanges()
+        {
+            throw new InvalidOperationException("This context is read-only");
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            throw new InvalidOperationException("This context is read-only");
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            throw new InvalidOperationException("This context is read-only");
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new InvalidOperationException("This context is read-only");
+        }
+
+        #endregion
+    }
+}
