9704d1cf0c0570462893e53e9a0d3bd07082d932
Jeremy Knight <jeremy.knight@cgb.com>
2020-06-23 10:01:49 -0500

    added tests for ReadOnlyDbContext

--- src/EntityFrameworkCore/JK.Common.EntityFrameworkCore/ReadOnlyDbContext.cs
+++ src/EntityFrameworkCore/JK.Common.EntityFrameworkCore/ReadOnlyDbContext.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace JK.Common.EntityFrameworkCore
 {
-    public abstract class ReadOnlyDbContext<TContext> : DbContext
+    public abstract class ReadOnlyDbContext : DbContext
     {
+        private const string readOnlyErrorMessage = "This context is read-only.";
+
         protected ReadOnlyDbContext()
         {
             ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
@@ -22,22 +24,22 @@ namespace JK.Common.EntityFrameworkCore
 
         public override int SaveChanges()
         {
-            throw new InvalidOperationException("This context is read-only");
+            throw new InvalidOperationException(readOnlyErrorMessage);
         }
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
         {
-            throw new InvalidOperationException("This context is read-only");
+            throw new InvalidOperationException(readOnlyErrorMessage);
         }
 
         public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
         {
-            throw new InvalidOperationException("This context is read-only");
+            throw new InvalidOperationException(readOnlyErrorMessage);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            throw new InvalidOperationException("This context is read-only");
+            throw new InvalidOperationException(readOnlyErrorMessage);
         }
 
         #endregion
