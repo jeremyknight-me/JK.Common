ed154222b41b6ae6bd3216a5439441c0daa6a819
Jeremy <jeremy@theknights.me>
2020-08-19 23:22:49 -0500

    added EquatableFacade

---
+++ src/JK.Common.Tests/EquatableFacadeTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+
+namespace JK.Common.Tests
+{
+    public class EquatableFacadeTests
+    {
+        [Fact]
+        public void AreEqual_BothNull_ReturnsTrue()
+        {
+            var sut = new EquatableFacade<SimpleObject>();
+            SimpleObject left = null;
+            SimpleObject right = null;
+            bool actual = sut.AreEqual(left, right);
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void AreEqual_LeftNullRightNotNull_ReturnsFalse()
+        {
+            var sut = new EquatableFacade<SimpleObject>();
+            SimpleObject left = null;
+            SimpleObject right = new SimpleObject();
+            bool actual = sut.AreEqual(left, right);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AreEqual_LeftNotNullRightNull_ReturnsFalse()
+        {
+            var sut = new EquatableFacade<SimpleObject>();
+            SimpleObject left = new SimpleObject();
+            SimpleObject right = null;
+            bool actual = sut.AreEqual(left, right);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AreEqual_SameReference_ReturnsTrue()
+        {
+            var sut = new EquatableFacade<SimpleObject>();
+            var left = new SimpleObject();
+            SimpleObject right = left;
+            bool actual = sut.AreEqual(left, right);
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [InlineData(1, "Title 1", 1, "Title 1", true)]
+        [InlineData(1, "Title 1", 2, "Title 2", false)]
+        public void AreEqual_Theories(int id1, string title1, int id2, string title2, bool expected)
+        {
+            var left = new SimpleObject { Id = id1, Title = title1 };
+            var right = new SimpleObject { Id = id2, Title = title2 };
+            var sut = new EquatableFacade<SimpleObject>();
+            sut.AreObjectsEqual = (left, right) => left.Id == right.Id && left.Title == right.Title;
+            bool actual = sut.AreEqual(left, right);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/JK.Common/EquatableFacade.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace JK.Common
+{
+    public sealed class EquatableFacade<T>
+    {
+        public Func<T, T, bool> AreObjectsEqual { get; set; } = (left, right) => ((T)left).Equals((T)right);
+
+        public bool AreEqual(object left, object right)
+        {
+            if (this.AreBothNullReference(left, right)
+                || this.AreSameReference(left, right))
+            {
+                return true;
+            }
+
+            if (this.AreEitherNullReference(left, right))
+            {
+                return false;
+            }
+
+            if (!(left is T))
+            {
+                throw new ArgumentException(@"The 'left' argument is not the correct type.", nameof(left));
+            }
+
+            if (!(right is T))
+            {
+                throw new ArgumentException(@"The 'right' argument is not the correct type.", nameof(right));
+            }
+
+            return this.AreObjectsEqual((T)left, (T)right);
+        }
+
+        private bool AreBothNullReference(object left, object right)
+        {
+            return this.IsNullReference(left) && this.IsNullReference(right);
+        }
+
+        private bool AreEitherNullReference(object left, object right)
+        {
+            return this.IsNullReference(left) || this.IsNullReference(right);
+        }
+
+        private bool AreSameReference(object left, object right)
+        {
+            return object.ReferenceEquals(left, right);
+        }
+
+        private bool IsNullReference(object entity)
+        {
+            return object.ReferenceEquals(entity, null);
+        }
+    }
+}
