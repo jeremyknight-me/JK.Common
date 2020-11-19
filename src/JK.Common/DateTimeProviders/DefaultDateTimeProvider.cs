abebe7e0eb56a40f187cf3717dad747a49759a7a
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2020-11-19 01:06:04 -0600

    updated date providers for dependency injection instead of singleton (#26)

--- src/JK.Common/DateTimeProviders/DefaultDateTimeProvider.cs
+++ src/JK.Common/DateTimeProviders/DefaultDateTimeProvider.cs
@@ -2,37 +2,10 @@
 
 namespace JK.Common.DateTimeProviders
 {
-    public sealed class DefaultDateTimeProvider : DateTimeProvider
+    public sealed class DefaultDateTimeProvider : IDateTimeProvider
     {
-        #region Singleton Pattern Implementation
-
-        private static readonly object threadLock = new object();
-
-        private static DefaultDateTimeProvider instance;
-
-        private DefaultDateTimeProvider()
-        {
-        }
-
-        public static DefaultDateTimeProvider Instance
-        {
-            get
-            {
-                lock (threadLock)
-                {
-                    if (instance == null)
-                    {
-                        instance = new DefaultDateTimeProvider();
-                    }
-                }
-
-                return instance;
-            }
-        }
-
-        #endregion
-
-        public override DateTime Now => DateTime.Now;
-        public override DateTime UtcNow => DateTime.UtcNow;
+        public DateTime Now => DateTime.Now;
+        public DateTime Today => DateTime.Today;
+        public DateTime UtcNow => DateTime.UtcNow;
     }
 }
---
+++ src/JK.Common/Patterns/Singleton.cs
@@ -0,0 +1,33 @@
+﻿namespace JK.Common.Patterns
+{
+    public sealed class Singleton<T> where T : new()
+    {
+        #region Singleton Pattern Implementation
+
+        private static readonly object threadLock = new object();
+
+        private static T instance;
+
+        private Singleton()
+        {
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                lock (threadLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new T();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+    }
+}
