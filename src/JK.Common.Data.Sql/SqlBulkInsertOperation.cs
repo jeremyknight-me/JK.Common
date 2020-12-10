6a2584b413a4e832805d9d7d5457bfd563900ca6
Jeremy <jeremy@theknights.me>
2020-12-10 00:11:24 -0600

    added sql bulk operation

---
+++ src/JK.Common.Data.Sql/SqlBulkInsertOperation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using JK.Common.Data.Ado;
+
+namespace JK.Common.Data.Sql
+{
+    public class SqlBulkInsertOperation<T> : OperationBase
+    {
+        public SqlBulkInsertOperation(DataContextBase context) : base(context)
+        {
+        }
+
+        public void Execute(SqlBulkCopySettings settings, IEnumerable<T> items)
+        {
+            #region ArgumentNull Checks
+
+            if (settings is null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            if (string.IsNullOrWhiteSpace(settings.TableName))
+            {
+                throw new ArgumentNullException(nameof(settings.TableName));
+            }
+
+            if (settings.Columns is null || settings.Columns.Count == 0)
+            {
+                throw new ArgumentNullException(nameof(settings.Columns));
+            }
+
+            #endregion
+
+            using (var connection = new SqlConnection(this.Context.ConnectionString))
+            using (var bulk = new SqlBulkCopy(connection, SqlBulkCopyOptions.KeepNulls | SqlBulkCopyOptions.UseInternalTransaction, null))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                bulk.BatchSize = settings.BatchSize == 0 ? items.Count() : settings.BatchSize;
+                bulk.DestinationTableName = settings.TableName;
+
+                var table = new DataTable();
+                var props = TypeDescriptor.GetProperties(typeof(T))
+                    .Cast<PropertyDescriptor>()
+                    .Where(pd => pd.PropertyType.Namespace.Equals("System")
+                        && settings.Columns.ContainsKey(pd.Name))
+                    .ToArray();
+                foreach (var prop in props)
+                {
+                    var columnName = settings.Columns[prop.Name];
+                    bulk.ColumnMappings.Add(prop.Name, columnName);
+                    table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+                }
+
+                var values = new object[props.Length];
+                foreach (var item in items)
+                {
+                    for (var i = 0; i < values.Length; i++)
+                    {
+                        values[i] = props[i].GetValue(item);
+                    }
+
+                    table.Rows.Add(values);
+                }
+
+                bulk.WriteToServer(table);
+            }
+        }
+
+        protected override void SetupParameters(IDbCommand command)
+        {
+        }
+    }
+}
