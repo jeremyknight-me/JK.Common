8844a792c0823500d1ebbb0da7d6105d9c7d8e37
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2021-11-15 20:53:38 -0600

    removed version directive in tests

--- src/JK.Common.Tests/Extensions/EnumerableExtensionsTests.cs
+++ src/JK.Common.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -5,91 +5,89 @@ namespace JK.Common.Tests.Extensions;
 
 public class EnumerableExtensionsTests
 {
-#if (!NET6_0_OR_GREATER)
     #region DistinctBy() Tests
 
-        [Fact]
-        public void DistinctByTestDate()
+    [Fact]
+    public void DistinctByTestDate()
+    {
+        var list = Enumerable.Range(0, 200).Select(i => new
         {
-            var list = Enumerable.Range(0, 200).Select(i => new
-            {
-                Index = i,
-                Date = DateTime.Today.AddDays(i % 4)
-            }).ToList();
+            Index = i,
+            Date = DateTime.Today.AddDays(i % 4)
+        }).ToList();
 
-            var distinctList = list.DistinctBy(l => l.Date).ToList();
+        var distinctList = list.DistinctBy(l => l.Date).ToList();
 
-            Assert.Equal(4, distinctList.Count);
+        Assert.Equal(4, distinctList.Count);
 
-            Assert.Equal(0, distinctList[0].Index);
-            Assert.Equal(1, distinctList[1].Index);
-            Assert.Equal(2, distinctList[2].Index);
-            Assert.Equal(3, distinctList[3].Index);
+        Assert.Equal(0, distinctList[0].Index);
+        Assert.Equal(1, distinctList[1].Index);
+        Assert.Equal(2, distinctList[2].Index);
+        Assert.Equal(3, distinctList[3].Index);
 
-            Assert.Equal(DateTime.Today, distinctList[0].Date);
-            Assert.Equal(DateTime.Today.AddDays(1), distinctList[1].Date);
-            Assert.Equal(DateTime.Today.AddDays(2), distinctList[2].Date);
-            Assert.Equal(DateTime.Today.AddDays(3), distinctList[3].Date);
+        Assert.Equal(DateTime.Today, distinctList[0].Date);
+        Assert.Equal(DateTime.Today.AddDays(1), distinctList[1].Date);
+        Assert.Equal(DateTime.Today.AddDays(2), distinctList[2].Date);
+        Assert.Equal(DateTime.Today.AddDays(3), distinctList[3].Date);
 
-            Assert.Equal(200, list.Count);
-        }
+        Assert.Equal(200, list.Count);
+    }
 
-        [Fact]
-        public void DistinctByTestInt()
+    [Fact]
+    public void DistinctByTestInt()
+    {
+        var list = Enumerable.Range(0, 200).Select(i => new
         {
-            var list = Enumerable.Range(0, 200).Select(i => new
-            {
-                Index = i % 4,
-                Date = DateTime.Today.AddDays(i)
-            }).ToList();
+            Index = i % 4,
+            Date = DateTime.Today.AddDays(i)
+        }).ToList();
 
-            var distinctList = list.DistinctBy(l => l.Index).ToList();
+        var distinctList = list.DistinctBy(l => l.Index).ToList();
 
-            Assert.Equal(4, distinctList.Count);
+        Assert.Equal(4, distinctList.Count);
 
-            Assert.Equal(0, distinctList[0].Index);
-            Assert.Equal(1, distinctList[1].Index);
-            Assert.Equal(2, distinctList[2].Index);
-            Assert.Equal(3, distinctList[3].Index);
+        Assert.Equal(0, distinctList[0].Index);
+        Assert.Equal(1, distinctList[1].Index);
+        Assert.Equal(2, distinctList[2].Index);
+        Assert.Equal(3, distinctList[3].Index);
 
-            Assert.Equal(DateTime.Today, distinctList[0].Date);
-            Assert.Equal(DateTime.Today.AddDays(1), distinctList[1].Date);
-            Assert.Equal(DateTime.Today.AddDays(2), distinctList[2].Date);
-            Assert.Equal(DateTime.Today.AddDays(3), distinctList[3].Date);
+        Assert.Equal(DateTime.Today, distinctList[0].Date);
+        Assert.Equal(DateTime.Today.AddDays(1), distinctList[1].Date);
+        Assert.Equal(DateTime.Today.AddDays(2), distinctList[2].Date);
+        Assert.Equal(DateTime.Today.AddDays(3), distinctList[3].Date);
 
-            Assert.Equal(200, list.Count);
-        }
+        Assert.Equal(200, list.Count);
+    }
 
-        [Fact]
-        public void TestStructs()
-        {
-            var list = Enumerable.Range(0, 200)
-                .Select(i => new EqualityStructTester(i, DateTime.Today.AddDays(i % 4)))
-                .ToList();
+    [Fact]
+    public void TestStructs()
+    {
+        var list = Enumerable.Range(0, 200)
+            .Select(i => new EqualityStructTester(i, DateTime.Today.AddDays(i % 4)))
+            .ToList();
 
-            var distinctDateList = list.DistinctBy(e => e.Date).ToList();
-            var distinctIntList = list.DistinctBy(e => e.Index).ToList();
+        var distinctDateList = list.DistinctBy(e => e.Date).ToList();
+        var distinctIntList = list.DistinctBy(e => e.Index).ToList();
 
-            Assert.Equal(4, distinctDateList.Count);
-            Assert.Equal(200, distinctIntList.Count);
-        }
+        Assert.Equal(4, distinctDateList.Count);
+        Assert.Equal(200, distinctIntList.Count);
+    }
 
-        [Fact]
-        public void TestClasses()
-        {
-            var list = Enumerable.Range(0, 200)
-                .Select(i => new EqualityClassTester(i, DateTime.Today.AddDays(i % 4)))
-                .ToList();
+    [Fact]
+    public void TestClasses()
+    {
+        var list = Enumerable.Range(0, 200)
+            .Select(i => new EqualityClassTester(i, DateTime.Today.AddDays(i % 4)))
+            .ToList();
 
-            var distinctDateList = list.DistinctBy(e => e.Date).ToList();
-            var distinctIntList = list.DistinctBy(e => e.Index).ToList();
+        var distinctDateList = list.DistinctBy(e => e.Date).ToList();
+        var distinctIntList = list.DistinctBy(e => e.Index).ToList();
 
-            Assert.Equal(4, distinctDateList.Count);
-            Assert.Equal(200, distinctIntList.Count);
-        }
+        Assert.Equal(4, distinctDateList.Count);
+        Assert.Equal(200, distinctIntList.Count);
+    }
 
     #endregion
-#endif
 
     #region AsIndexedEnumerable() Tests
 
