5a737bb33e7da7581de33370fad3ce5309d43204
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2021-12-29 10:50:17 -0600

    added Lazy<T> Singleton pattern implementation (#54)

---
+++ src/JK.Common/Patterns/Singleton/LazySingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JK.Common.Patterns;
+
+/// <summary>Example of singleton pattern LazyT implementation</summary>
+internal sealed class LazySingleton
+{
+    private static readonly Lazy<LazySingleton> lazy = new Lazy<LazySingleton>(() => new LazySingleton());
+
+    private LazySingleton()
+    {
+    }
+
+    public static LazySingleton Instance => lazy.Value;
+}
---
+++ src/JK.Common/Patterns/Singleton/LockedSingleton.cs
@@ -0,0 +1,26 @@
+﻿namespace JK.Common.Patterns.Singleton;
+
+internal sealed class LockedSingleton
+{
+    private static LockedSingleton instance = null;
+    private static readonly object threadLock = new object();
+
+    private LockedSingleton()
+    {
+    }
+
+    public static LockedSingleton Instance
+    {
+        get
+        {
+            lock (threadLock)
+            {
+                if (instance == null)
+                {
+                    instance = new LockedSingleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
