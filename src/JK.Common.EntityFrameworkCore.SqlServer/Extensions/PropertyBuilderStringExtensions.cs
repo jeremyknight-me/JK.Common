fc018a95cffa88d7ad432b7fea782c1d0eb24cfb
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2022-01-12 09:55:45 -0600

    added hasMaxLength and IsUnicode calls as needed (#56)

--- src/JK.Common.EntityFrameworkCore.SqlServer/Extensions/PropertyBuilderStringExtensions.cs
+++ src/JK.Common.EntityFrameworkCore.SqlServer/Extensions/PropertyBuilderStringExtensions.cs
@@ -5,14 +5,14 @@ namespace JK.Common.EntityFrameworkCore.SqlServer.Extensions;
 public static class PropertyBuilderStringExtensions
 {
     public static PropertyBuilder<string> HasColumnTypeNvarchar(this PropertyBuilder<string> propertyBuilder)
-        => propertyBuilder.HasColumnType("nvarchar(max)");
+        => propertyBuilder.HasColumnType("nvarchar(max)").IsUnicode();
 
     public static PropertyBuilder<string> HasColumnTypeNvarchar(this PropertyBuilder<string> propertyBuilder, int length)
-        => propertyBuilder.HasColumnType($"nvarchar({length})");
+        => propertyBuilder.HasColumnType($"nvarchar({length})").HasMaxLength(length).IsUnicode();
 
     public static PropertyBuilder<string> HasColumnTypeVarchar(this PropertyBuilder<string> propertyBuilder)
-        => propertyBuilder.HasColumnType("varchar(max)");
+        => propertyBuilder.HasColumnType("varchar(max)").IsUnicode(false);
 
     public static PropertyBuilder<string> HasColumnTypeVarchar(this PropertyBuilder<string> propertyBuilder, int length)
-        => propertyBuilder.HasColumnType($"varchar({length})");
+        => propertyBuilder.HasColumnType($"varchar({length})").HasMaxLength(length).IsUnicode(false);
 }
