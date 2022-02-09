0a2677fa04859c7089f7613266b087456a58b1d7
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2022-02-09 00:14:35 -0600

    implemented insertion sort

---
+++ src/JK.Common/Sorting/InsertionSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JK.Common.Sorting;
+
+public static class InsertionSort
+{
+    public static void Sort<T>(IList<T> list) where T : IComparable, IComparable<T>
+    {
+        for (var i = 0; i < list.Count - 1; i++)
+        {
+            for (var j = i + 1; j > 0; j--)
+            {
+                if (list[j - 1].CompareTo(list[j]) > 0)
+                {
+                    var temp = list[j - 1];
+                    list[j - 1] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+    }
+}
