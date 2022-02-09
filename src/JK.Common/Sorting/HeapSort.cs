2b5b1e77bd70b6ea5573f14b2ad3825ffabb6aa6
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2022-02-09 00:38:37 -0600

    implemented heap sort algorithm

---
+++ src/JK.Common/Sorting/HeapSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JK.Common.Sorting
+{
+    public static class HeapSort
+    {
+        public static void Sort<T>(IList<T> list) where T : IComparable, IComparable<T>
+        {
+            MakeHeap(list, list.Count);
+            for (var i = list.Count - 1; i > 0; i--)
+            {
+                var value = list[i];
+                list[i] = list[0];
+                list[0] = value;
+                MakeHeap(list, i);
+            }
+        }
+
+        private static void MakeHeap<T>(IList<T> list, int c)
+            where T : IComparable, IComparable<T>
+        {
+            for (var i = 1; i < c; i++)
+            {
+                var value = list[i];
+                var s = i;
+                var f = (s - 1) / 2;
+                while (s > 0 && list[f].CompareTo(value) < 0)
+                {
+                    list[s] = list[f];
+                    s = f;
+                    f = (s - 1) / 2;
+                }
+                list[s] = value;
+            }
+        }
+    }
+}
