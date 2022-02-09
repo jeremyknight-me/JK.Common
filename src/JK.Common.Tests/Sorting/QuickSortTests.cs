196dee7a1a09b5858a4d66cd4cd717554ab26489
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2022-02-08 22:21:01 -0600

    added int quick sort

---
+++ src/JK.Common.Tests/Sorting/QuickSortTests.cs
@@ -0,0 +1,40 @@
+﻿using JK.Common.Sorting;
+
+namespace JK.Common.Tests.Sorting;
+
+public class QuickSortTests
+{
+    [Fact]
+    public void Sort_Test1()
+    {
+        var original = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+        QuickSort.Sort(original, 0, original.Length - 1);
+        Assert.Collection(original,
+            i => Assert.Equal(-4, i),
+            i => Assert.Equal(0, i),
+            i => Assert.Equal(2, i),
+            i => Assert.Equal(5, i),
+            i => Assert.Equal(6, i),
+            i => Assert.Equal(11, i),
+            i => Assert.Equal(18, i),
+            i => Assert.Equal(22, i),
+            i => Assert.Equal(51, i),
+            i => Assert.Equal(67, i)
+        );
+    }
+
+    [Fact]
+    public void Sort_Test2()
+    {
+        var original = new int[] { 9, 7, 5, 3, 10, 6 };
+        QuickSort.Sort(original, 0, original.Length - 1);
+        Assert.Collection(original,
+            i => Assert.Equal(3, i),
+            i => Assert.Equal(5, i),
+            i => Assert.Equal(6, i),
+            i => Assert.Equal(7, i),
+            i => Assert.Equal(9, i),
+            i => Assert.Equal(10, i)
+        );
+    }
+}
---
+++ src/JK.Common/Sorting/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿namespace JK.Common.Sorting;
+
+public class QuickSort
+{
+    public static void Sort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            var partitionIndex = Partition(arr, low, high);
+            Sort(arr, low, partitionIndex - 1);
+            Sort(arr, partitionIndex + 1, high);
+        }
+    }
+
+    private static int Partition(int[] arr, int low, int high)
+    {
+        var pivot = arr[low];
+        while (high > low)
+        {
+            var highValue = arr[high];
+            while (pivot < highValue)
+            {
+                if (high <= low)
+                {
+                    break;
+                }
+                high--;
+                highValue = arr[high];
+            }
+
+            arr[low] = highValue;
+            var lowValue = arr[low];
+            while (pivot > lowValue)
+            {
+                if (high <= low)
+                {
+                    break;
+                }
+                low++;
+                lowValue = arr[low];
+            }
+
+            arr[high] = lowValue;
+        }
+
+        arr[low] = pivot;
+        return low;
+    }
+}
