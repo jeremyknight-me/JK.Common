0b5d4f3e55ec3cf208fba6a70a33b94cfde85850
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-02-10 18:29:26 -0600

    changed decimal helper to decimal instead of long

--- src/JK.Common/Extensions/DecimalExtensions.cs
+++ src/JK.Common/Extensions/DecimalExtensions.cs
@@ -8,10 +8,10 @@ public static class DecimalExtensions
     /// <summary>Gets the decimal part (right side of decimal) from the given decimal.</summary>
     /// <param name="value">Current Decimal object from extension method.</param>
     /// <returns>Right side of decimal as integer</returns>
-    public static long GetDecimalPart(this decimal value) => DecimalHelper.GetDecimalPart(value);
+    public static decimal GetDecimalPart(this decimal value) => DecimalHelper.GetDecimalPart(value);
 
     /// <summary>Gets the whole part (left side of decimal) from the given decimal.</summary>
     /// <param name="value">Current Decimal object from extension method.</param>
     /// <returns>Left side of decimal as integer</returns>
-    public static long GetWholePart(this decimal value) => DecimalHelper.GetWholePart(value);
+    public static decimal GetWholePart(this decimal value) => DecimalHelper.GetWholePart(value);
 }
--- src/JK.Common/TypeHelpers/DecimalHelper.cs
+++ src/JK.Common/TypeHelpers/DecimalHelper.cs
@@ -7,16 +7,11 @@ public static class DecimalHelper
 {
     /// <summary>Gets the decimal part (right side of decimal) from the given decimal.</summary>
     /// <param name="value">Decimal value</param>
-    /// <returns>Right side of decimal as integer</returns>
-    public static long GetDecimalPart(in decimal value)
-    {
-        var decimalPart = value - Math.Truncate(value);
-        var right = $"{decimalPart}".Replace("0.", "").TrimEnd('0');
-        return string.IsNullOrWhiteSpace(right) ? 0 : long.Parse(right);
-    }
+    /// <returns>Right side of decimal</returns>
+    public static decimal GetDecimalPart(in decimal value) => value - Math.Truncate(value);
 
     /// <summary>Gets the whole part (left side of decimal) from the given decimal.</summary>
     /// <param name="value">Decimal value</param>
-    /// <returns>Left side of decimal as integer</returns>
-    public static long GetWholePart(in decimal value) => decimal.ToInt64(Math.Truncate(value));
+    /// <returns>Left side of decimal</returns>
+    public static decimal GetWholePart(in decimal value) => decimal.ToInt64(Math.Truncate(value));
 }
