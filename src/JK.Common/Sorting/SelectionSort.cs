788776d21b9c71e155f1bc63a109c6035482fd49
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2022-02-10 23:31:39 -0600

    implemented selection sort

---
+++ src/JK.Common/Sorting/SelectionSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JK.Common.Sorting;
+
+public static class SelectionSort
+{
+    public static void Sort<T>(IList<T> list) where T : IComparable, IComparable<T>
+    {
+        for (var i = 0; i <= list.Count - 2; i++)
+        {
+            for (var j = i + 1; j <= list.Count - 1; j++)
+            {
+                if (list[i].CompareTo(list[j]) > 0)
+                {
+                    var temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+    }
+}
