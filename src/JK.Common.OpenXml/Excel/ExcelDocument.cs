285c2976a333bae025b1942c7c0d5c56757d982a
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-04-21 23:11:35 -0500

    added cell extensions and cell value parser

--- src/JK.Common.OpenXml/Excel/ExcelDocument.cs
+++ src/JK.Common.OpenXml/Excel/ExcelDocument.cs
@@ -11,10 +11,16 @@ namespace JK.Common.OpenXml.Excel;
 public class ExcelDocument : IDisposable
 {
     private SpreadsheetDocument spreadsheet;
+    private WorkbookPart workbookPart;
 
     internal ExcelDocument(SpreadsheetDocument spreadsheetDocument)
     {
         this.spreadsheet = spreadsheetDocument;
+        if (spreadsheetDocument != null)
+        {
+            this.workbookPart = this.spreadsheet.WorkbookPart;
+        }
+        
     }
 
     public static ExcelDocument Load(string path, bool editable = false)
@@ -33,12 +39,19 @@ public class ExcelDocument : IDisposable
 
     public void Dispose()
     {
-        this.spreadsheet.Dispose();   
+        this.workbookPart = null;
+        this.spreadsheet.Dispose();
     }
 
     public DataSet GetDataSet()
     {
-        return null;
+        var sheets = this.workbookPart.GetNamedWorksheets();
+        var set = new DataSet();
+        foreach (var sheet in sheets)
+        {
+            var table = this.BuildDataTable(sheet.Value, sheet.Key);
+        }
+        return set;
     }
 
     public DataTable GetDataTableBySheetName(string sheetName, int headerRows = 0)
@@ -51,15 +64,47 @@ public class ExcelDocument : IDisposable
         var workbookPart = this.spreadsheet.WorkbookPart;
         var sheet = workbookPart.GetSheetByName(sheetName);
         var worksheet = sheet.GetWorksheet(workbookPart);
-        var rows = worksheet.Elements<SheetData>().First().Elements<Row>().ToArray();
+        return this.BuildDataTable(worksheet, sheetName, headerRows);
+    }
 
+    private DataTable BuildDataTable(Worksheet worksheet, string sheetName, int headerRows = 0)
+    {
+        var rows = worksheet.Elements<SheetData>().First().Elements<Row>().ToArray();
         var table = new DataTable(sheetName);
         if (!rows.Any())
         {
-            
+            return table;
         }
 
-        //return GetDataTableFromRows(workbookPart, rows, sheetName, headerRows);
-        return null;
+        var columnCells = rows.ElementAt(0).Cast<Cell>();
+        int columnIndex = 0;
+        foreach (var cell in columnCells)
+        {
+            int cellColumnIndex = cell.GetColumnIndex();
+            while (columnIndex < cellColumnIndex)
+            {
+                table.Columns.Add($"Column{columnIndex + 1}");
+                columnIndex++;
+            }
+
+            var value = cell.GetValue(workbookPart);
+            var valueColumnName = string.IsNullOrWhiteSpace(value)
+                ? $"Column{columnIndex + 1}"
+                : $"Column{columnIndex + 1}_{value}";
+            table.Rows.Add(valueColumnName);
+            columnIndex++;
+        }
+
+        //foreach (var row in rows)
+        //{
+        //    ProcessRow(row, dataTable, workbookPart);
+        //}
+
+        //for (int i = 0; i < headerRows; i++)
+        //{
+        //    dataTable.Rows.RemoveAt(0);
+        //}
+
+        return table;
     }
 }
---
+++ src/JK.Common.OpenXml/Excel/Extensions/CellExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace JK.Common.OpenXml.Excel.Extensions
+{
+    internal static class CellExtensions
+    {
+        /// <summary>
+        /// Given a cell, parses the specified cell to get the zero based column index.
+        /// </summary>
+        /// <param name="cell">Cell with reference (ie. C8)</param>
+        /// <returns>Column index (ie. 2)</returns>
+        internal static int GetColumnIndex(this CellType cell)
+        {
+            var number = cell.GetColumnNumber();
+            return number - 1;
+        }
+
+        /// <summary>
+        /// Given a cell, parses the specified cell to get the column letter.
+        /// </summary>
+        /// <param name="cell">Cell with reference (ie. B2)</param>
+        /// <returns>Column letter (ie. B)</returns>       
+        internal static string GetColumnLetter(this CellType cell)
+        {
+            var reference = cell.CellReference;
+            if (string.IsNullOrWhiteSpace(reference))
+            {
+                throw new ArgumentException("CellReference must be valid reference.");
+            }
+
+            var regex = new Regex("[A-Za-z]+"); // Create a regular expression to match the column name portion of the cell name.
+            var match = regex.Match(reference);
+            return match.Value;
+        }
+
+        /// <summary>
+        /// Given a cell, parses the specified cell to get the column number.
+        /// </summary>
+        /// <param name="cell">Cell with reference (ie. C8)</param>
+        /// <returns>Column number (ie. 3)</returns>
+        internal static int GetColumnNumber(this CellType cell)
+        {
+            var letter = cell.GetColumnLetter();
+            if (string.IsNullOrWhiteSpace(letter))
+            {
+                throw new ArgumentException("CellReference must be valid reference.");
+            }
+
+            letter = letter.ToUpperInvariant();
+            var sum = 0;
+            foreach (var c in letter.ToCharArray())
+            {
+                sum *= 26;
+                sum += (c - 'A' + 1);
+            }
+            return sum;
+        }
+
+        internal static string GetValue(this CellType cell, WorkbookPart workbookPart)
+            => CellValueParser.GetValue(workbookPart, cell);
+    }
+}
