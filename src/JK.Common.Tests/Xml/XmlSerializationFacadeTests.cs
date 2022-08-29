ac574661bf32f3e9c70c77ec21a70d4a671a69a8
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-08-29 15:18:53 -0500

    added unit test for xml serialization

---
+++ src/JK.Common.Tests/Xml/XmlSerializationFacadeTests.cs
@@ -0,0 +1,17 @@
+﻿using JK.Common.Xml;
+
+namespace JK.Common.Tests.Xml
+{
+    public class XmlSerializationFacadeTests
+    {
+        [Fact]
+        public void GetXmlAsString_Test()
+        {
+            var simple = new SimpleObject { Id = 1, Title = "ABC" };
+            var sut = new XmlSerializationFacade();
+            var actual = sut.GetXmlAsString(simple);
+            var expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?><SimpleObject xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><Id>1</Id><Title>ABC</Title></SimpleObject>";
+            Assert.Equal(expected, actual);
+        }
+    }
+}
