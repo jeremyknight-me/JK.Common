7f7fae3f79f3275efbb5c5ec245c2c37b7499adf
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-11-14 15:29:38 -0600

    added guid parameter extensions

---
+++ src/JK.Common.Data.Sql.Tests/Extensions/Parameters/UniqueIdentifierParameterTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using JK.Common.Data.Sql.Extensions.Parameters;
+using Microsoft.Data.SqlClient;
+using Xunit;
+
+namespace JK.Common.Data.Sql.Tests.Extensions.SqlParameterCollection;
+
+public class UniqueIdentifierParameterTests
+{
+    [Theory]
+    [MemberData(nameof(AddAlways_Data))]
+    public void AddAlways_Theories(string name, Guid value)
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddAlways(name, value);
+        var parameter = ParameterAssertHelper.AssertSingleAndReturn(command, name);
+        Assert.Equal(value, parameter.Value);
+        this.AssertDbTypes(parameter);
+    }
+
+    [Fact]
+    public void AddAlways_Null_Tests()
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddAlways("foo", (Guid?)null);
+        var parameter = ParameterAssertHelper.AssertSingleAndReturn(command, "foo");
+        ParameterAssertHelper.AssertDbNull(parameter);
+        this.AssertDbTypes(parameter);
+    }
+
+    [Theory]
+    [MemberData(nameof(AddAlways_Data))]
+    public void AddIfNonNull_NonNull_Theories(string name, Guid? value)
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddIfNonNull(name, value);
+        var parameter = ParameterAssertHelper.AssertSingleAndReturn(command, name);
+        Assert.Equal(value, parameter.Value);
+        this.AssertDbTypes(parameter);
+    }
+
+    [Fact]
+    public void AddIfNonNull_Null_Test()
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddIfNonNull("hi", (Guid?)null);
+        Assert.Empty(command.Parameters);
+    }
+
+    public static IEnumerable<object[]> AddAlways_Data()
+    {
+        yield return new object[] { "Foo", Guid.Parse("9df7c774-027e-4ae8-bf82-c158052987f7") };
+        yield return new object[] { "Bar", Guid.Parse("ca6293b4-deb6-4d45-b119-c892d5e1c549") };
+    }
+
+    private void AssertDbTypes(SqlParameter parameter)
+    {
+        Assert.Equal(DbType.Guid, parameter.DbType);
+        Assert.Equal(SqlDbType.UniqueIdentifier, parameter.SqlDbType);
+    }
+}
---
+++ src/JK.Common.Data.Sql/Extensions/Parameters/UniqueIdentifierParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace JK.Common.Data.Sql.Extensions.Parameters;
+
+public static class UniqueIdentifierParameter
+{
+    public static SqlParameterCollection AddAlways(this SqlParameterCollection parameters, string name, Guid value)
+    {
+        parameters.Add(name, SqlDbType.UniqueIdentifier).Value = value;
+        return parameters;
+    }
+
+    public static SqlParameterCollection AddAlways(this SqlParameterCollection parameters, string name, Guid? value)
+    {
+        parameters.Add(name, SqlDbType.UniqueIdentifier).Value = value.HasValue ? value : DBNull.Value;
+        return parameters;
+    }
+
+    public static SqlParameterCollection AddIfNonNull(this SqlParameterCollection parameters, string name, Guid? value)
+        => parameters.AddIfNonNull(name, SqlDbType.UniqueIdentifier, value);
+}
