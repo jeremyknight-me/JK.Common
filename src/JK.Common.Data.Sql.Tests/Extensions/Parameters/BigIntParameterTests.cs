d45b4273185c59fe689de71337f4e4d3461ade7c
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-11-14 15:23:00 -0600

    added big and small int parameter extensions

---
+++ src/JK.Common.Data.Sql.Tests/Extensions/Parameters/BigIntParameterTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using JK.Common.Data.Sql.Extensions.Parameters;
+using Microsoft.Data.SqlClient;
+using Xunit;
+
+namespace JK.Common.Data.Sql.Tests.Extensions.SqlParameterCollection;
+
+public class BigIntParameterTests
+{
+    [Theory]
+    [InlineData("Foo", 1)]
+    [InlineData("Bar", 2)]
+    public void AddAlways_Theories(string name, long value)
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddAlways(name, value);
+        var parameter = ParameterAssertHelper.AssertSingleAndReturn(command, name);
+        Assert.Equal(value, parameter.Value);
+        this.AssertDbType(parameter);
+    }
+
+    [Fact]
+    public void AddAlways_Null_Tests()
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddAlways("foo", (long?)null);
+        var parameter = ParameterAssertHelper.AssertSingleAndReturn(command, "foo");
+        ParameterAssertHelper.AssertDbNull(parameter);
+        this.AssertDbType(parameter);
+    }
+
+    [Theory]
+    [InlineData("Foo", 1)]
+    [InlineData("Bar", 2)]
+    public void AddIfNonNull_NonNull_Theories(string name, long? value)
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddIfNonNull(name, value);
+        var parameter = ParameterAssertHelper.AssertSingleAndReturn(command, name);
+        Assert.Equal(value, parameter.Value);
+        this.AssertDbType(parameter);
+    }
+
+    [Fact]
+    public void AddIfNonNull_Null_Test()
+    {
+        using var command = new SqlCommand();
+        command.Parameters.AddIfNonNull("hi", (long?)null);
+        Assert.Empty(command.Parameters);
+    }
+
+    private void AssertDbType(SqlParameter parameter)
+    {
+        Assert.Equal(DbType.Int64, parameter.DbType);
+        Assert.Equal(SqlDbType.BigInt, parameter.SqlDbType);
+    }
+}
---
+++ src/JK.Common.Data.Sql/Extensions/Parameters/BigIntParameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace JK.Common.Data.Sql.Extensions.Parameters;
+
+public static class BigIntParameters
+{
+    public static SqlParameterCollection AddAlways(this SqlParameterCollection parameters, string name, long value)
+    {
+        parameters.Add(name, SqlDbType.BigInt).Value = value;
+        return parameters;
+    }
+
+    public static SqlParameterCollection AddAlways(this SqlParameterCollection parameters, string name, long? value)
+    {
+        parameters.Add(name, SqlDbType.BigInt).Value = value.HasValue ? value : DBNull.Value;
+        return parameters;
+    }
+
+    public static SqlParameterCollection AddIfNonNull(this SqlParameterCollection parameters, string name, long? value)
+        => parameters.AddIfNonNull(name, SqlDbType.BigInt, value);
+}
+
