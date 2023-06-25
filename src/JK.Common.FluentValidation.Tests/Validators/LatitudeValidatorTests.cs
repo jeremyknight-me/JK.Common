41ed0212f4fbbb9cebe182afddc29f13b31848a0
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-06-25 02:03:07 -0500

    updated lat/long validator tests to use decimal instead of double

--- src/JK.Common.FluentValidation.Tests/Validators/LatitudeValidatorTests.cs
+++ src/JK.Common.FluentValidation.Tests/Validators/LatitudeValidatorTests.cs
@@ -1,25 +1,25 @@
 ﻿namespace JK.Common.FluentValidation.Tests.Validators;
 
-public class LatitudeValidatorTests : DoubleValidatorTestsBase
+public class LatitudeValidatorTests : DecimalValidatorTestsBase
 {
-    public override PropertyValidator<MockModel, double> Validator => new LatitudeValidator<MockModel, double>();
+    public override PropertyValidator<MockModel, decimal> Validator => new LatitudeValidator<MockModel, decimal>();
 
     [Theory]
     [InlineData(-90)]
     [InlineData(0)]
     [InlineData(90)]
-    public void IsValid_TrueTheories(double value)
+    public void IsValid_TrueTheories(decimal value)
     {
         var result = this.MakeAndTestValidator(value);
-        result.ShouldNotHaveValidationErrorFor(x => x.DoubleValue);
+        result.ShouldNotHaveValidationErrorFor(x => x.DecimalValue);
     }
 
     [Theory]
     [InlineData(-91)]
     [InlineData(91)]
-    public void IsValid_FalseTheories(double value)
+    public void IsValid_FalseTheories(decimal value)
     {
         var result = this.MakeAndTestValidator(value);
-        result.ShouldHaveValidationErrorFor(x => x.DoubleValue);
+        result.ShouldHaveValidationErrorFor(x => x.DecimalValue);
     }
 }
