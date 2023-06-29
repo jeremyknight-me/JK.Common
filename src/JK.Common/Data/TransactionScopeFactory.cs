eef16178c11c7ce9d46d96f1002f8b117e272534
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-06-29 16:19:43 -0500

    added TransactionScopeFactory

---
+++ src/JK.Common/Data/TransactionScopeFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Transactions;
+
+namespace JK.Common.Data;
+
+public static class TransactionScopeFactory
+{
+    public static TransactionScope Create()
+        => Create(IsolationLevel.ReadCommitted);
+
+    public static TransactionScope Create(IsolationLevel isolationLevel)
+    {
+        var options = new TransactionOptions
+        {
+            IsolationLevel = isolationLevel,
+            Timeout = TransactionManager.MaximumTimeout
+        };
+        return new TransactionScope(TransactionScopeOption.Required, options);
+    }
+}
