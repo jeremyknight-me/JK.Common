01ebf6c35daa3bb3b949b2a305497137c537fc1e
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-09-22 12:10:23 -0500

    added UnsafeSqlExceptionFactory

---
+++ src/JK.Common.Data.Sql/TestUtils/UnsafeSqlExceptionFactory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
+
+namespace JK.Common.Data.Sql.TestUtils;
+
+/// <summary>
+/// WARNING: This is meant for unit testing only!!!
+/// </summary>
+public static class UnsafeSqlExceptionFactory
+{
+    public static SqlException Make(int number, string message)
+    {
+        var error = MakeSqlError(number, message);
+        var collection = MakeSqlErrorCollection(error);
+        var exception = MakeSqlException(collection);
+        return exception;
+    }
+
+    private static SqlException MakeSqlException(SqlErrorCollection collection)
+    {
+        var exceptionType = typeof(SqlException);
+        var createMethod = exceptionType.GetMethod(
+            "CreateException",
+            BindingFlags.NonPublic | BindingFlags.Static,
+            new Type[] { typeof(SqlErrorCollection), typeof(string) }
+        );
+        var exception = createMethod.Invoke(null, new object[] { collection, "TESTING" }) as SqlException;
+        return exception;
+    }
+
+    private static SqlErrorCollection MakeSqlErrorCollection(SqlError error)
+    {
+        var collectionType = typeof(SqlErrorCollection);
+        var ctor = collectionType.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, Array.Empty<Type>());
+        var collection = ctor.Invoke(Array.Empty<object>()) as SqlErrorCollection;
+        var addMethod = collectionType.GetMethod("Add", BindingFlags.NonPublic | BindingFlags.Instance);
+        addMethod.Invoke(collection, new object[] { error });
+        return collection;
+    }
+
+    private static SqlError MakeSqlError(int number, string message)
+    {
+        var errorType = typeof(SqlError);
+        var error = Instantiate<SqlError>();
+        var privateBindings = BindingFlags.NonPublic | BindingFlags.Instance;
+
+        var numberMember = errorType.GetField("_number", privateBindings);
+        numberMember.SetValue(error, number);
+
+        if (!string.IsNullOrWhiteSpace(message))
+        {
+            var messageMember = errorType.GetField("_message", privateBindings);
+            messageMember.SetValue(error, message);
+        }
+
+        return error;
+    }
+
+    /// <summary>
+    /// BEWARE: GetUninitializedObject doesn't call the constructor.
+    /// </summary>
+    private static T Instantiate<T>() where T : class
+        => FormatterServices.GetUninitializedObject(typeof(T)) as T;
+}
