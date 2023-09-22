d7de7a9567c18dfc1fc4bac4f6783b63f92c6722
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-09-22 12:11:43 -0500

    added sql exception extensions

---
+++ src/JK.Common.Data.Sql/Extensions/SqlExceptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace JK.Common.Data.Sql.Extensions;
+
+public static class SqlExceptionExtensions
+{
+    public static bool IsTimeoutError(this SqlException exception)
+        => exception.Number == -2 || exception.ContainsTimeoutText();
+
+    private static bool ContainsTimeoutText(this SqlException exception)
+        => !string.IsNullOrWhiteSpace(exception.Message)
+            && exception.Message.ToLower().Contains("timeout");
+}
