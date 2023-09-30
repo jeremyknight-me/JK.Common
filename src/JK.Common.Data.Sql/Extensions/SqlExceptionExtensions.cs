4c06ad7e5e2f55231cb2aa6b87fadbdf6df81458
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-09-30 09:35:23 -0500

    added UnsafeSqlExceptionFactory and IsTimeoutError extension method (#93)
    * added UnsafeSqlExceptionFactory
    * added sql exception extensions
    * updated sql changelog

---
+++ src/JK.Common.Data.Sql/Extensions/SqlExceptionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace JK.Common.Data.Sql.Extensions;
+
+public static class SqlExceptionExtensions
+{
+    public static bool IsTimeoutError(this SqlException exception)
+        => exception.Number == -2;
+}
