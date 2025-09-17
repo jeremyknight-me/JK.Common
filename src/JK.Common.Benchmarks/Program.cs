f28a1d814d97f0bb5d63699275f193fbdbede92e
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-09-17 10:54:04 -0500

    added benchmarks project for JK.Common

---
+++ src/JK.Common.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace JK.Common.Benchmarks;
+
+internal class Program
+{
+    static void Main(string[] args)
+        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
