3498f42575691befae09e7966259be6dbf2eff1c
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-11-13 12:47:30 -0600

    renamed properties for better readability

--- src/JK.Common/Extensions/DateFormatExtensions.cs
+++ src/JK.Common/Extensions/DateFormatExtensions.cs
@@ -12,49 +12,49 @@ public static class DateFormatExtensions
         /// full date and long time string representation.
         /// </summary>
         /// <returns>Date and time string in the format: Sunday, January 31, 2010 12:45:30 PM</returns>
-        public string ToFormatFullDateLongTime() => $"{date:f}";
+        public string ToFullDateLongTimeFormat() => $"{date:f}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// full date and short time string representation.
         /// </summary>
         /// <returns>Date and time string in the format: Sunday, January 31, 2010 12:45 PM</returns>
-        public string ToFormatFullDateShortTime() => $"{date:f}";
+        public string ToFullDateShortTimeFormat() => $"{date:f}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// general date and short time string representation.
         /// </summary>
         /// <returns>Date and time string in the format: 1/31/2010 12:45 PM</returns>
-        public string ToFormatGeneralDateShortTime() => $"{date:g}";
+        public string ToGeneralDateShortTimeFormat() => $"{date:g}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// month and day string representation.
         /// </summary>
         /// <returns>Date and time string in the format: January 31</returns>
-        public string ToFormatMonthDay() => $"{date:M}";
+        public string ToMonthDayFormat() => $"{date:M}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// RFC1123 string representation.
         /// </summary>
         /// <returns>Date and time string in the format: Sun, 31 Jan 2010 12:45:30 GMT</returns>
-        public string ToFormatRfc1123() => $"{date:R}";
+        public string ToRfc1123Format() => $"{date:R}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// sortable date and time string representation.
         /// </summary>
         /// <returns>Date and time string in the format: 2010-01-31T12:45:30</returns>
-        public string ToFormatSortableDateTime() => $"{date:s}";
+        public string ToSortableFormat() => $"{date:s}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// universal sortable date and time string representation.
         /// </summary>
         /// <returns>Date and time string in the format: 2010-01-31 12:45:30Z</returns>
-        public string ToFormatUniversalSortable() => $"{date:u}";
+        public string ToUniversalSortableFormat() => $"{date:u}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
@@ -62,13 +62,13 @@ public static class DateFormatExtensions
         /// </summary>
         /// <returns>Date and time string in the format: Sunday, January 31, 2010 6:45:30 PM
         /// (This takes into account time zone and returns UTC time)</returns>
-        public string ToFormatUniversal() => $"{date:U}";
+        public string ToUniversalFormat() => $"{date:U}";
 
         /// <summary>
         /// Formats the value of the current DateTime object to its equivalent 
         /// year and month string representation.
         /// </summary>
         /// <returns>Date and time string in the format: January, 2010</returns>
-        public string ToFormatYearMonth() => $"{date:Y}";
+        public string ToYearMonthFormat() => $"{date:Y}";
     }
 }
