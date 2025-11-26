170046f3b841de7b9959494c6848338cbe7ce979
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-11-26 16:43:00 -0600

    polyfill generator library (#126)
    * added additional attributes to IsExternalInit
    * added polyfill generator projects
    * updated nuget packages

---
+++ src/JK.Common.PolyfillGenerators.Tests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using Microsoft.CodeAnalysis.CSharp.Testing;
+global using Microsoft.CodeAnalysis.Testing;
+global using Xunit;
--- src/JK.Common/IsExternalInit.cs
+++ src/JK.Common/IsExternalInit.cs
@@ -1,11 +1,15 @@
 ﻿#if !NET5_0_OR_GREATER
 
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace System.Runtime.CompilerServices;
 #pragma warning restore IDE0130 // Namespace does not match folder structure
 
+[DebuggerNonUserCode]
+[ExcludeFromCodeCoverage]
 [EditorBrowsable(EditorBrowsableState.Never)]
 internal static class IsExternalInit
 {
