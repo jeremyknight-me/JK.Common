4f72852100f186e6d9f140740551fe6c7693a135
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-11-30 20:24:44 -0600

    added enums for testing

---
+++ src/JK.Common.Generators.EnumHelpers.Tests/ColorEnums.cs
@@ -0,0 +1,50 @@
+﻿namespace JK.Common.Generators.EnumHelpers.Tests;
+
+public enum ByteColors : byte
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
+
+public enum IntColors
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
+
+public enum LongColors : long
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
+
+public enum ShortColors : short
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
+
+public enum UIntColors : uint
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
+
+public enum ULongColors : ulong
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
+
+public enum UShortColors : ushort
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
