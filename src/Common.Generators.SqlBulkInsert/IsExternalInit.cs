b00ab7491389218b986a2b9e344c7f2845ceebad
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2026-07-11 23:30:40 -0500

    added new projects

---
+++ src/Common.Generators.SqlBulkInsert/IsExternalInit.cs
@@ -0,0 +1,18 @@
+﻿#if !NET5_0_OR_GREATER
+
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System.Runtime.CompilerServices;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+[DebuggerNonUserCode]
+[ExcludeFromCodeCoverage]
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class IsExternalInit
+{
+}
+
+#endif
