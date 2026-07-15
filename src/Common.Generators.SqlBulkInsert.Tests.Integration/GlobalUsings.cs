3b94889e8065bbab9b6daaad8f959207224ba8e4
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2026-07-14 23:44:08 -0500

    added SqlBulkCopy source generation library (#135)
    Introduced a Roslyn source generator using attributes to generate bulk insert code for annotated types. Added supporting types, extensions, and unit tests. Marked legacy SqlBulkCopy types as obsolete and updated dependencies.
    
    * Add source generator for SQL bulk insert operations
    * added integration project
    * added integration tests

---
+++ src/Common.Generators.SqlBulkInsert.Tests.Integration/GlobalUsings.cs
@@ -0,0 +1,2 @@
+global using Microsoft.Data.SqlClient;
+global using Xunit;
---
+++ src/Common.Generators.SqlBulkInsert.Tests.Integration/MsSqlContainerFixture.cs
@@ -0,0 +1,40 @@
+using System.Data.Common;
+using Testcontainers.MsSql;
+using Testcontainers.Xunit;
+using Xunit.Sdk;
+
+namespace JK.Common.Generators.SqlBulkInsert.Tests;
+
+public sealed class MsSqlContainerFixture : DbContainerFixture<MsSqlBuilder, MsSqlContainer>
+{
+    public MsSqlContainerFixture(IMessageSink messageSink) 
+        : base(messageSink)
+    {
+    }
+
+    public override DbProviderFactory DbProviderFactory
+        => SqlClientFactory.Instance;
+
+    protected override MsSqlBuilder Configure()
+        => new("mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04");
+
+    protected override async ValueTask InitializeAsync()
+    {
+        await base.InitializeAsync();
+
+        await using SqlConnection connection = (SqlConnection)await OpenConnectionAsync(TestContext.Current.CancellationToken);
+        using SqlCommand command = connection.CreateCommand();
+        command.CommandText = "CREATE TABLE Employees (FirstName NVARCHAR(100), LastName NVARCHAR(100), Age INT)";
+        await command.ExecuteNonQueryAsync(TestContext.Current.CancellationToken);
+    }
+
+    protected override async ValueTask DisposeAsyncCore()
+    {
+        await using SqlConnection connection = (SqlConnection)await OpenConnectionAsync(TestContext.Current.CancellationToken);
+        using SqlCommand command = connection.CreateCommand();
+        command.CommandText = "DROP TABLE IF EXISTS Employees";
+        await command.ExecuteNonQueryAsync(TestContext.Current.CancellationToken);
+
+        await base.DisposeAsyncCore();
+    }
+}
